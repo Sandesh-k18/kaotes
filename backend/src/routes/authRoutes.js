9886293ff1d04d7d4d4b5c12021cb3b3a3fe3528
Sandesh-k18 <sandeshkharel18@gmail.com>
2025-12-25 23:58:57 +0545

    simple kaotes with oauth

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,21 +2,28 @@ import express from "express";
 import passport from "passport";
 import { protect } from "../middleware/authMiddleware.js";
 import { googleAuthCallback } from "../controllers/authController.js";
-
+import rateLimiter from "../middleware/rateLimiter.js";
 
 const router = express.Router();
 
 // This starts the Google Login process
-router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
 // This is where Google redirects the user after they log in
-router.get("/google/callback", 
-  passport.authenticate("google", { session: false, failureRedirect: "/login" }),
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    session: false,
+    failureRedirect: "/login",
+  }),
   googleAuthCallback
 );
 
 // routes/authRoutes.js
-router.get("/me", protect, (req, res) => {
+router.get("/me", protect, rateLimiter, (req, res) => {
   try {
     if (!req.user) {
       return res.status(404).json({ message: "User not found" });
